25278fa0daba437d712df302153f60b4892f046a
SvenJuergens <SvenJuergens@users.noreply.github.com>
2020-10-04 21:04:33 +0200

    [TASK] cleanup

--- Classes/Widgets/WeatherWidget.php
+++ Classes/Widgets/WeatherWidget.php
@@ -19,26 +19,13 @@ namespace SvenJuergens\WeatherWidget\Widgets;
 
 use TYPO3\CMS\Core\Cache\Frontend\FrontendInterface as Cache;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Dashboard\Widgets\AdditionalCssInterface;
 use TYPO3\CMS\Dashboard\Widgets\ButtonProviderInterface;
 use TYPO3\CMS\Dashboard\Widgets\WidgetConfigurationInterface;
 use TYPO3\CMS\Dashboard\Widgets\WidgetInterface;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 
-/**
- * Concrete RSS widget implementation
- *
- * The widget will show a certain number of items of the given RSS feed. The feed will be set by the feedUrl option. You
- * can add a button to the widget by defining a button provider.
- *
- * The following options are available during registration:
- * - feedUrl        string                      Defines the URL or file providing the RSS Feed.
- *                                              This is read by the widget in order to fetch entries to show.
- * - limit          int     default: 5          Defines how many RSS items should be shown.
- * - lifetime       int     default: 43200      Defines how long to wait, in seconds, until fetching RSS Feed again
- *
- * @see ButtonProviderInterface
- */
-class WeatherWidget implements WidgetInterface
+class WeatherWidget implements WidgetInterface, AdditionalCssInterface
 {
     /**
      * @var WidgetConfigurationInterface
@@ -77,11 +64,11 @@ class WeatherWidget implements WidgetInterface
         $this->cache = $cache;
         $this->options = array_merge(
             [
-                'lifeTime' => 43200
+                'lifeTime' => 3600,
+                'language' => 'de'
             ],
             $options
         );
-        $this->buttonProvider = $buttonProvider;
     }
 
     public function renderWidgetContent(): string
@@ -90,7 +77,6 @@ class WeatherWidget implements WidgetInterface
         $this->view->assignMultiple([
             'weather' =>  $this->getWeather() ?: '',
             'options' => $this->options,
-            'button' => $this->buttonProvider,
             'configuration' => $this->configuration,
         ]);
         return $this->view->render();
@@ -99,9 +85,7 @@ class WeatherWidget implements WidgetInterface
 
     protected function getWeather(): array
     {
-
-        $url = 'https://de.wttr.in/' . $this->getLocation() .  '?format=%c|%C|%t|%w|%l';
-
+        $url = 'https://' . $this->options['language'] . '.wttr.in/' . $this->getLocation() .  '?format=%c|%C|%t|%w|%l';
      #   $cacheHash = md5($url);
      #   if ($weatherDetails = $this->cache->get($cacheHash)) {
      #       return $weatherDetails;
@@ -121,6 +105,12 @@ class WeatherWidget implements WidgetInterface
 
     protected function getLocation(): string
     {
-        return 'Berlin';
+        return $this->options['location'];
     }
+
+    public function getCssFiles(): array
+    {
+        return ['EXT:weather_widget/Resources/Public/Css/weatherWidget.css'];
+    }
+
 }
