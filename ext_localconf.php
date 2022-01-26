919321c91878d4022888c063b809dbd6c69b2761
Sven Jürgens <SvenJuergens@users.noreply.github.com>
2022-01-26 17:29:00 +0100

    [BUGFIX]  Warning: Undefined array key with PHP8

--- ext_localconf.php
+++ ext_localconf.php
@@ -16,7 +16,7 @@ module.tx_dashboard {
     }
 '));
 
-if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['dashboard_weather'])) {
+if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['dashboard_weather'] ?? null)) {
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['dashboard_weather'] = [
         'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
         'backend' => \TYPO3\CMS\Core\Cache\Backend\FileBackend::class,
